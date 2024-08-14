0848e7f3003163ae21ebf74b865f55f2431b0eab
raphaelDkhn <113879115+raphaelDkhn@users.noreply.github.com>
2024-08-14 16:52:59 +0300

    mul ops

--- crates/luminal_cairo/cairo_programs/projects/mul/src/lib.cairo
+++ crates/luminal_cairo/cairo_programs/projects/mul/src/lib.cairo
@@ -1,6 +1,6 @@
 use orion_numbers::{F64, F64Impl};
-use orion_dl::{Tensor, tensor_add, BinaryOpMetadata};
+use orion_dl::{Tensor, tensor_mul, BinaryOpMetadata};
 
 fn main(x: Tensor<F64>, y: Tensor<F64>, mut metadata: BinaryOpMetadata) -> Tensor<F64> {
-    tensor_add(x, y, ref metadata)
+    tensor_mul(x, y, ref metadata)
 }
--- crates/luminal_cairo/src/tests/binary.rs
+++ crates/luminal_cairo/src/tests/binary.rs
@@ -25,6 +25,35 @@ fn test_add() {
 
     let _ = cx.compile(cairo_compiler, &mut d);
     cx.execute();
-
+    
+    assert_eq!(d.shape.shape_usize(), vec![2, 2]);
     assert_close(&d.data(), &[12.0, 23.0, 34.0, 45.0])
+}
+
+#[test]
+fn test_mul() {
+    let mut cx = Graph::new();
+    let a = cx.tensor((2, 2)).set([[1.0, 2.0, 3.0, 4.0]]);
+    let b = cx.tensor((2, 2)).set([[10.0, 20.0, 30.0, 40.0]]);
+    let c = a * b;
+    let bias = cx.tensor((2, 2)).set([[2.0, 2.0, 2.0, 2.0]]);
+    let mut d = (c * bias).retrieve();
+
+    let config = CairoRunnerConfig {
+        trace_file: None,
+        memory_file: None,
+        proof_mode: false,
+        air_public_input: None,
+        air_private_input: None,
+        cairo_pie_output: None,
+        append_return_values: false,
+    };
+
+    let cairo_compiler = CairoCompiler::new(config);
+
+    let _ = cx.compile(cairo_compiler, &mut d);
+    cx.execute();
+
+    assert_eq!(d.shape.shape_usize(), vec![2, 2]);
+    assert_close(&d.data(), &[20.0, 80.0, 180.0, 320.0])
 }
